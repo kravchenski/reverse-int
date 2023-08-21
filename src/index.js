08117505e16632faf2932963065a7431a81e353f
kravchenski <max5045955@gmail.com>
2023-08-22 00:47:06 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function reverse (n) {
-  
-}
+    let reverseInt = String(n).replace('-', '')
+    let newInt = ''
+    if (reverseInt[reverseInt - 1] == '-'){
+        for (let i = 1; i <= reverseInt.length; i++) {
+            newInt += reverseInt[reverseInt.length - i]
+    
+        }
+        return newInt
+
+    }
+    else{
+        for (let i = 1; i <= reverseInt.length; i++) {
+            newInt += reverseInt[reverseInt.length - i]
+    
+        }
+    }
+
+    return newInt
+}  
+
